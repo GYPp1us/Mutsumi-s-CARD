85efe3ac68c70d4c57cbe15792ab5138d2342e16
GYPp1us <gypplus@outlook.com>
2026-07-28 22:07:37 +0800

    发布版本号至v0.6.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.mutsumi.card"
         minSdk = 26
         targetSdk = 36
-        versionCode = 24
-        versionName = "0.6.5"
+        versionCode = 25
+        versionName = "0.6.6"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
